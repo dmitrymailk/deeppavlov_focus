07f2cdcef34eaff50852ad3f33d1ae09a50c8093
dmitrymailk <dmitrymail_k@mail.ru>
2022-09-19 23:01:01 +0300

    add commit validation

--- train-bart.sh
+++ train-bart.sh
@@ -1,2 +1,9 @@
+git_status=$(git status -s) 
+
+if [ -n "$git_status" ]; then
+    echo "You have uncommitted changes. Please commit them first."
+    exit 1
+fi
+
 train_log_path=bart_base_$(date +"%d.%m.%Y_%H:%M:%S").log
 nohup python train_bart_LM.py --is_debug=1 > ./training_logs/$train_log_path &

da910d7a4651dc88865d3bb5e6fd18d29514f882
dmitrymailk <dmitrymail_k@mail.ru>
2022-09-20 21:36:37 +0300

    fix types for dataloaders

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 ```bash
 unzip -o -d ./datasets/FoCus.zip ./datasets/
 ```
+
+### Subtasks
+#### Subtask 1
+- **Goal**: Predicting the proper persona sentences and knowledge 
+- **Input**: Persona candidates (5 sentences), Knowledge candidates (10 paragraphs), document on the topic, and dialog history
+- **Output**: Index of answer persona sentences, Index of answer knowledge
+- **Evaluation**: Accuracy
+#### Subtask 2
+- **Goal**: Generating the next agent response in natural language using persona and knowledge
+- **Input**: Persona sentences, document on the topic, and dialog history
+- **Output**: Agent utterance
+- **Evaluation**: CharF++, BLEU, ROUGE-L
--- core/dataloaders/focus_dataloader.py
+++ core/dataloaders/focus_dataloader.py
@@ -1,7 +1,7 @@
 import json
 import os
 from itertools import chain
-from typing import Dict, List, TypedDict
+from typing import Dict, List, TypedDict, cast
 
 from core.hyperparameters.bart_hyperparameters import BartHyperparametersV1
 from core.tokenizers.bart_tokenizers import BartFoCusTokenizerV1
@@ -14,10 +14,23 @@ from torch.utils.data import DataLoader, Dataset
 
 
 class FoCusDatasetSampleDictV1(TypedDict):
+    # fmt: off
+    """
+        persona: List[str] список предложений из персоны
+        knowledge_candidates: List[str] список кандидатов с негативными примерами
+            и одним правильным
+        persona_grounding: List[int] маска которая указывает какие предложения из
+            персоны использовались
+        dialog: List[int] пары диалогов истории
+        knowledge_answer_index: int индекс правильного ответа из кандидатов
+        knowledge: List[str] все знания об объекте из википедии что у нас есть
+
+    """
+    # fmt: on
     persona: List[str]
     knowledge_candidates: List[str]
     persona_grounding: List[int]
-    dialog: List[int]
+    dialog: List[List[int]]
     knowledge_answer_index: int
     knowledge: List[str]
 
@@ -49,14 +62,16 @@ class FoCusDatasetSampleV1:
         self.knowledge = knowledge
 
     def get_dict(self) -> FoCusDatasetSampleDictV1:
-        return {
-            "persona": self.persona,
-            "knowledge_candidates": self.knowledge_candidates,
-            "persona_grounding": self.persona_grounding,
-            "dialog": self.dialog,
-            "knowledge_answer_index": self.knowledge_answer_index,
-            "knowledge": self.knowledge,
-        }
+        return FoCusDatasetSampleDictV1(
+            **{
+                "persona": self.persona,
+                "knowledge_candidates": self.knowledge_candidates,
+                "persona_grounding": self.persona_grounding,
+                "dialog": self.dialog,
+                "knowledge_answer_index": self.knowledge_answer_index,
+                "knowledge": self.knowledge,
+            },
+        )
 
 
 class BartFoCusDatasetSampleDictV1(TypedDict):
@@ -65,22 +80,11 @@ class BartFoCusDatasetSampleDictV1(TypedDict):
 
 
 class BartFoCusDatasetSampleV1:
-    # fmt: off
-    """
-        [BOS][persona][SEP][knowledge][SEP][dialog][:-1][SEP]<dialog>[dialog][-1]</dialog>
-        - [dialog] - набор диалоговых пар
-        - persona - все предложения персоны
-        - knowledge - топ наиболее похожих предложений из knowledge к контексту диалога
-        - [dialog][:-1] - все диалоговые пары, кроме ответа бота
-        - <dialog>[dialog][-1]</dialog> - ответ бота
-    """
-    # fmt: on
-
     def __init__(
         self,
-        focus_dataset_sample: FoCusDatasetSampleDictV1 = None,
-        tokenizer: BartFoCusTokenizerV1 = None,
-        h_params: BartHyperparametersV1 = None,
+        focus_dataset_sample: FoCusDatasetSampleDictV1,
+        tokenizer: BartFoCusTokenizerV1,
+        h_params: BartHyperparametersV1,
     ) -> None:
         self.focus_dataset_sample = focus_dataset_sample
         self.tokenizer = tokenizer
@@ -96,17 +100,26 @@ class BartFoCusDatasetSampleV1:
         self.dialog_eos = self.__get_token_id(h_params.dialog_eos_token)
 
     def __get_token_id(self, token: str) -> int:
-        return self.tokenizer.convert_tokens_to_ids(token)
+        token_index = self.tokenizer.get_vocab().get(token, self.unk_token_id)
+        return token_index
 
-    def __flat_list(self, list_of_lists: List[List]) -> List:
+    def __flat_list(self, list_of_lists: List[List]) -> List[int]:
         return list(chain.from_iterable(list_of_lists))
 
     def get_dict(self) -> BartFoCusDatasetSampleDictV1:
+        """
+        [BOS][persona][SEP][knowledge][SEP][dialog][:-1][SEP]<dialog>[dialog][-1]</dialog>
+        - [dialog] - набор диалоговых пар
+        - persona - все предложения персоны
+        - knowledge - топ наиболее похожих предложений из knowledge к контексту диалога
+        - [dialog][:-1] - все диалоговые пары, кроме ответа бота
+        - <dialog>[dialog][-1]</dialog> - ответ бота
+        """
         dialog_history_length = self.h_params.dialog_history_length
         context_length = self.h_params.context_length
 
         persona = self.focus_dataset_sample["persona"]
-        dialog = self.focus_dataset_sample["dialog"]
+        dialog: List[List[int]] = self.focus_dataset_sample["dialog"]
         knowledge = self.focus_dataset_sample["knowledge"]
 
         encoded_persona = self.tokenizer.batch_encode_plus(
@@ -128,7 +141,9 @@ class BartFoCusDatasetSampleV1:
         )
 
         # контекст на основе которого подбирается knowledge
-        query_context = dialog_history_feature["input_ids"][-context_length:]
+        query_context = dialog_history_feature["input_ids"][  # type: ignore
+            -context_length:
+        ]  # noqa: E501
         encoded_knowledge = self.tokenizer.batch_encode_plus(
             knowledge,
             add_special_tokens=False,
@@ -141,10 +156,14 @@ class BartFoCusDatasetSampleV1:
         )
 
         # [BOS][persona][SEP][knowledge][SEP][dialog][:-1][SEP]<dialog>[dialog][-1]</dialog>
-        flat_persona = self.__flat_list(encoded_persona["input_ids"])
+        flat_persona = self.__flat_list(encoded_persona["input_ids"])  # type: ignore
         flat_knowledge = self.__flat_list(most_similar_knowledge)
-        flat_dialog_history = self.__flat_list(dialog_history_feature["input_ids"])
-        flat_bot_response = self.__flat_list(dialog_history_target["input_ids"])
+        flat_dialog_history = self.__flat_list(
+            dialog_history_feature["input_ids"],  # type: ignore
+        )
+        flat_bot_response = self.__flat_list(
+            dialog_history_target["input_ids"],  # type: ignore
+        )
 
         flat_persona = flat_persona[: self.h_params.max_persona_tokens]
         flat_knowledge = flat_knowledge[: self.h_params.max_knowledge_tokens]
@@ -177,7 +196,7 @@ class BartFoCusDatasetSampleV1:
 class FoCusDatasetV1:
     def __init__(
         self,
-        input_dataset_path: str = None,
+        input_dataset_path: str,
     ) -> None:
         assert input_dataset_path is not None, "input_dataset_path is None"
 
@@ -192,7 +211,7 @@ class FoCusDatasetV1:
 
     def __create_initial_dataset(
         self,
-        initial_dataset: Dict = None,
+        initial_dataset: Dict,
     ) -> List[FoCusDatasetSampleDictV1]:
         dataset = []
         initial_dataset_data = initial_dataset["data"]
@@ -224,7 +243,7 @@ class FoCusDatasetV1:
 
         return dataset
 
-    def __read_dataset(self, input_path: str) -> list:
+    def __read_dataset(self, input_path: str) -> Dict:
         with open(input_path, "r") as f:
             dataset = json.load(f)
         return dataset
@@ -240,8 +259,8 @@ class PytorchFoCusDatasetV1(Dataset):
     def __init__(
         self,
         dataset: FoCusDatasetV1,
-        tokenizer: BartFoCusTokenizerV1 = None,
-        hyperparameters: BartHyperparametersV1 = None,
+        tokenizer: BartFoCusTokenizerV1,
+        hyperparameters: BartHyperparametersV1,
     ) -> None:
         self.dataset = dataset
         self.hyperparameters = hyperparameters
@@ -250,7 +269,7 @@ class PytorchFoCusDatasetV1(Dataset):
     def __len__(self) -> int:
         return len(self.dataset)
 
-    def __getitem__(self, index: int) -> FoCusDatasetSampleDictV1:
+    def __getitem__(self, index: int) -> BartFoCusDatasetSampleDictV1:
         dataset_sample = self.dataset[index]
         train_sample = BartFoCusDatasetSampleV1(
             focus_dataset_sample=dataset_sample,
@@ -258,16 +277,17 @@ class PytorchFoCusDatasetV1(Dataset):
             h_params=self.hyperparameters,
         )
         train_sample = train_sample.get_dict()
+        train_sample = BartFoCusDatasetSampleDictV1(**train_sample)
         return train_sample
 
 
 class FoCusLightningDataModuleV1(LightningDataModule):
     def __init__(
         self,
-        train_path_dataset: str = None,
-        valid_path_dataset: str = None,
-        hyperparameters: BartHyperparametersV1 = None,
-        tokenizer: BartFoCusTokenizerV1 = None,
+        train_path_dataset: str,
+        valid_path_dataset: str,
+        hyperparameters: BartHyperparametersV1,
+        tokenizer: BartFoCusTokenizerV1,
         is_debug: bool = False,
     ) -> None:
         super().__init__()
@@ -275,29 +295,26 @@ class FoCusLightningDataModuleV1(LightningDataModule):
         self.train_path_dataset = train_path_dataset
         self.valid_path_dataset = valid_path_dataset
 
-        self.train_dataset = None
-        self.valid_dataset = None
-
         self.hyperparameters = hyperparameters
         self.tokenizer = tokenizer
 
         self.is_debug = is_debug
 
-    def setup(self, stage: str = None) -> None:
+    def setup(self, stage: str) -> None:
         train_dataset = FoCusDatasetV1(input_dataset_path=self.train_path_dataset)
         valid_dataset = FoCusDatasetV1(input_dataset_path=self.valid_path_dataset)
 
         if self.is_debug:
-            train_dataset = train_dataset[:2]
-            valid_dataset = valid_dataset[:2]
+            train_dataset = train_dataset[:2]  # type: ignore
+            valid_dataset = valid_dataset[:2]  # type: ignore
 
         self.train_dataset = PytorchFoCusDatasetV1(
-            dataset=train_dataset,
+            dataset=train_dataset,  # type: ignore
             tokenizer=self.tokenizer,
             hyperparameters=self.hyperparameters,
         )
         self.valid_dataset = PytorchFoCusDatasetV1(
-            dataset=valid_dataset,
+            dataset=valid_dataset,  # type: ignore
             tokenizer=self.tokenizer,
             hyperparameters=self.hyperparameters,
         )
@@ -307,7 +324,7 @@ class FoCusLightningDataModuleV1(LightningDataModule):
             self.train_dataset,
             batch_size=self.hyperparameters.train_batch_size,
             shuffle=True,
-            num_workers=os.cpu_count(),
+            num_workers=os.cpu_count(),  # type: ignore
             collate_fn=self.collate_fn,
         )
 
@@ -316,11 +333,11 @@ class FoCusLightningDataModuleV1(LightningDataModule):
             self.valid_dataset,
             batch_size=self.hyperparameters.valid_batch_size,
             shuffle=False,
-            num_workers=os.cpu_count(),
+            num_workers=os.cpu_count(),  # type: ignore
             collate_fn=self.collate_fn,
         )
 
-    def collate_fn(self, batch: List[FoCusDatasetSampleDictV1]) -> Dict:
+    def collate_fn(self, batch: List[BartFoCusDatasetSampleDictV1]) -> Dict:
         max_len = 0
         for item in batch:
             max_len = max(max_len, len(item["input_ids"]))
@@ -329,10 +346,13 @@ class FoCusLightningDataModuleV1(LightningDataModule):
         pad_attention_mask = []
 
         for item in batch:
-            input_ids: List = item["input_ids"]
-            attention_mask: List = item["attention_mask"]
+            input_ids = item["input_ids"]
+            attention_mask = item["attention_mask"]
 
-            pad_tokens = [self.tokenizer.pad_token_id] * (max_len - len(input_ids))
+            pad_tokens = cast(
+                List[int],
+                [self.tokenizer.pad_token_id] * (max_len - len(input_ids)),
+            )
             pad_attention = [0] * (max_len - len(attention_mask))
 
             input_ids.extend(pad_tokens)
@@ -345,3 +365,170 @@ class FoCusLightningDataModuleV1(LightningDataModule):
             "input_ids": torch.tensor(pad_input_ids),
             "attention_mask": torch.tensor(pad_attention_mask),
         }
+
+
+class BartFoCusDatasetSampleDictV2(TypedDict):
+    input_ids: List[int]
+    attention_mask: List[int]
+    persona_grounding: List[int]
+    knowledge_answer_index: int
+    knowledge_sep_index: int
+    dialog_bos_index: int
+    dialog_eos_index: int
+
+
+class BartFoCusDatasetSampleV2:
+    def __init__(
+        self,
+        focus_dataset_sample: FoCusDatasetSampleDictV1,
+        tokenizer: BartFoCusTokenizerV1,
+        h_params: BartHyperparametersV1,
+    ) -> None:
+        self.focus_dataset_sample = focus_dataset_sample
+        self.tokenizer = tokenizer
+        self.h_params = h_params
+
+        self.bos_token_id = self.tokenizer.bos_token_id
+        self.pad_token_id = self.tokenizer.pad_token_id
+        self.unk_token_id = self.tokenizer.unk_token_id
+        self.sep_token_id = self.tokenizer.sep_token_id
+        self.cls_token_id = self.tokenizer.cls_token_id
+
+        self.dialog_bos = self.__get_token_id(h_params.dialog_bos_token)
+        self.dialog_eos = self.__get_token_id(h_params.dialog_eos_token)
+
+    def __get_token_id(self, token: str) -> int:
+        token_index = self.tokenizer.get_vocab().get(token, self.unk_token_id)
+        return token_index
+
+    def __flat_list(self, list_of_lists: List[List]) -> List:
+        return list(chain.from_iterable(list_of_lists))
+
+    def get_dict(self) -> BartFoCusDatasetSampleDictV2:
+        """
+        Returns:
+            input_ids (List[int]):
+                [BOS][persona][SEP][knowledge][SEP][dialog][:-1][SEP]<dialog>[dialog][-1]</dialog>
+                - [dialog] - набор диалоговых пар
+                - persona - все предложения персоны
+                - knowledge - топ наиболее похожих предложений из knowledge к
+                    контексту диалога
+                - [dialog][:-1] - все диалоговые пары, кроме ответа бота
+                - <dialog>[dialog][-1]</dialog> - ответ бота
+            attention_mask (List[int]): маска для input_ids
+            persona_grounding (List[int]): маска которая показывает какие предложения
+                из взяты персоны
+            knowledge_answer_index (int): индекс ответа бота в knowledge_kanditates
+            knowledge_sep_index (int): индекс токена SEP между knowledge и dialog
+            dialog_bos_index (int): индекс токена <dialog>
+            dialog_eos_index (int): индекс токена </dialog>
+        """
+
+        dialog_history_length = self.h_params.dialog_history_length
+        context_length = self.h_params.context_length
+
+        persona = self.focus_dataset_sample["persona"]
+        dialog = self.focus_dataset_sample["dialog"]
+        knowledge = self.focus_dataset_sample["knowledge"]
+        persona_grounding = self.focus_dataset_sample["persona_grounding"]
+        knowledge_answer_index = self.focus_dataset_sample["knowledge_answer_index"]
+
+        encoded_persona = self.tokenizer.batch_encode_plus(
+            persona,
+            add_special_tokens=False,
+            truncation=True,
+        )
+
+        dialog_history = dialog[-2 * dialog_history_length :]
+        dialog_history_feature = self.tokenizer.batch_encode_plus(
+            dialog_history[:-1],
+            add_special_tokens=False,
+            truncation=True,
+        )
+        dialog_history_target = self.tokenizer.batch_encode_plus(
+            dialog_history[-1:],
+            add_special_tokens=False,
+            truncation=True,
+        )
+
+        # контекст на основе которого подбирается knowledge
+        query_context = dialog_history_feature["input_ids"][  # type: ignore
+            -context_length:
+        ]
+        encoded_knowledge = self.tokenizer.batch_encode_plus(
+            knowledge,
+            add_special_tokens=False,
+            truncation=True,
+        )
+
+        tf_idf = FoCusTfIdf(corpus=encoded_knowledge["input_ids"])
+        most_similar_knowledge = tf_idf.top_similar(
+            query=query_context,
+        )
+
+        # [BOS][persona][SEP][knowledge][SEP][dialog][:-1][SEP]<dialog>[dialog][-1]</dialog>
+        flat_persona = self.__flat_list(encoded_persona["input_ids"])  # type: ignore
+        flat_knowledge = self.__flat_list(most_similar_knowledge)
+        flat_dialog_history = self.__flat_list(
+            dialog_history_feature["input_ids"],  # type: ignore
+        )
+        flat_bot_response = self.__flat_list(
+            dialog_history_target["input_ids"],  # type: ignore
+        )
+
+        flat_persona = flat_persona[: self.h_params.max_persona_tokens]
+        flat_knowledge = flat_knowledge[: self.h_params.max_knowledge_tokens]
+        flat_dialog_history = flat_dialog_history[
+            : self.h_params.max_dialog_history_tokens
+        ]
+        flat_bot_response = flat_bot_response[: self.h_params.max_bot_response_tokens]
+
+        input_sequence = [
+            self.bos_token_id,
+            *flat_persona,
+            self.sep_token_id,
+            *flat_knowledge,
+            self.sep_token_id,
+            *flat_dialog_history,
+            self.sep_token_id,
+            self.dialog_bos,
+            *flat_bot_response,
+            self.dialog_eos,
+        ]
+
+        attention_mask = [1] * len(input_sequence)
+        knowledge_sep_index = 1 + len(flat_persona) + 1 + len(flat_knowledge)
+        dialog_bos_index = knowledge_sep_index + 1 + len(flat_dialog_history)
+        dialog_eos_index = dialog_bos_index + len(flat_bot_response)
+
+        return {
+            "input_ids": input_sequence,
+            "attention_mask": attention_mask,
+            "persona_grounding": persona_grounding,
+            "knowledge_answer_index": knowledge_answer_index,
+            "knowledge_sep_index": knowledge_sep_index,
+            "dialog_bos_index": dialog_bos_index,
+            "dialog_eos_index": dialog_eos_index,
+        }
+
+
+class PytorchFoCusDatasetV2(PytorchFoCusDatasetV1):
+    def __init__(
+        self,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+
+    def __len__(self) -> int:
+        return len(self.dataset)
+
+    def __getitem__(self, index: int) -> BartFoCusDatasetSampleDictV2:
+        dataset_sample = self.dataset[index]
+        train_sample = BartFoCusDatasetSampleV2(
+            focus_dataset_sample=dataset_sample,
+            tokenizer=self.tokenizer,
+            h_params=self.hyperparameters,
+        )
+        train_sample = train_sample.get_dict()
+        train_sample = BartFoCusDatasetSampleDictV2(**train_sample)
+        return train_sample

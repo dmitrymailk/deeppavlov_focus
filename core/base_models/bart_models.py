d5dbc20edff92b39ebe1000f248ec30cf8c6a155
dmitrymailk <dmitrymail_k@mail.ru>
2022-09-28 02:06:49 +0300

    solve inheritance for generation

--- core/base_models/bart_models.py
+++ core/base_models/bart_models.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from ast import Tuple
+from typing import List, Optional, Union
 
 from core.hyperparameters.bart_hyperparameters import (
     BartHyperparametersV1,
@@ -10,8 +11,14 @@ from core.tokenizers.bart_tokenizers import BartFoCusTokenizerV1, BartFoCusToken
 import torch
 from torch import nn
 
-from transformers import BartConfig, BartModel, BartPretrainedModel  # type: ignore
 from transformers.modeling_outputs import Seq2SeqLMOutput
+from transformers.models.bart.configuration_bart import BartConfig
+from transformers.models.bart.modeling_bart import (
+    BartForConditionalGeneration,
+    BartModel,
+    BartPretrainedModel,
+)
+from transformers.models.bart.modeling_bart import shift_tokens_right
 from transformers.utils import ModelOutput  # type: ignore
 
 
@@ -992,3 +999,91 @@ class BartLMV6(BartPretrainedModel):
 
     def get_decoder(self):
         return self.model.get_decoder()
+
+
+class BartLMV7(BartForConditionalGeneration):
+    base_model_prefix = "model"
+    _keys_to_ignore_on_load_missing = [r"final_logits_bias", r"lm_head.weight"]
+
+    def __init__(
+        self,
+        config: BartConfig,
+        hyperparameters: BartHyperparametersV3,
+        tokenizer: BartFoCusTokenizerV2,
+    ):
+        super().__init__(config)
+        self.model = BartModel(config)
+        self.tokenizer = tokenizer
+        self.hyperparameters = hyperparameters
+        self.register_buffer("final_logits_bias", torch.zeros((1, len(tokenizer))))
+        self.lm_head = nn.Linear(config.d_model, len(tokenizer), bias=False)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def forward(
+        self,
+        input_ids: torch.LongTensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        decoder_input_ids: Optional[torch.LongTensor] = None,
+        decoder_attention_mask: Optional[torch.LongTensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        decoder_head_mask: Optional[torch.Tensor] = None,
+        cross_attn_head_mask: Optional[torch.Tensor] = None,
+        encoder_outputs: Optional[List[torch.FloatTensor]] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        decoder_inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple, Seq2SeqLMOutput]:
+
+        if labels is not None:
+
+            use_cache = False
+            if decoder_input_ids is None and decoder_inputs_embeds is None:
+                decoder_input_ids = shift_tokens_right(
+                    labels,  # type: ignore
+                    self.config.pad_token_id,  # type: ignore
+                    self.config.decoder_start_token_id,  # type: ignore
+                )
+
+        outputs = self.model(
+            input_ids,
+            attention_mask=attention_mask,
+            decoder_input_ids=decoder_input_ids,
+            encoder_outputs=encoder_outputs,
+            decoder_attention_mask=decoder_attention_mask,
+            head_mask=head_mask,
+            decoder_head_mask=decoder_head_mask,
+            cross_attn_head_mask=cross_attn_head_mask,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            decoder_inputs_embeds=decoder_inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+        )
+        lm_logits = self.lm_head(outputs[0])
+
+        masked_lm_loss = None
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            masked_lm_loss = loss_fct(
+                lm_logits.view(-1, self.config.vocab_size), labels.view(-1)
+            )
+
+        return Seq2SeqLMOutput(
+            loss=masked_lm_loss,
+            logits=lm_logits,
+            past_key_values=outputs.past_key_values,
+            decoder_hidden_states=outputs.decoder_hidden_states,
+            decoder_attentions=outputs.decoder_attentions,
+            cross_attentions=outputs.cross_attentions,
+            encoder_last_hidden_state=outputs.encoder_last_hidden_state,
+            encoder_hidden_states=outputs.encoder_hidden_states,
+            encoder_attentions=outputs.encoder_attentions,
+        )

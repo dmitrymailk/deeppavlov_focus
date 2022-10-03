98a2c947f769b1f98dfd7c4c914d4a3e2cf934d5
dmitrymailk <dmitrymail_k@mail.ru>
2022-10-03 13:42:35 +0300

    update train script

--- core/train_scripts/debertav3_training.py
+++ core/train_scripts/debertav3_training.py
@@ -1,6 +1,8 @@
-from core.experiments.debertav3_experiments import (
+from core.experiments.debertav3_experiments import (  # noqa: F401
     experiment_1,
+    experiment_2,
 )
 
 if __name__ == "__main__":
-    experiment_1()
+    # experiment_1()
+    experiment_2()

0f419f34f5df088833ebab7e9f1e4a9b383278b6
dmitrymailk <dmitrymail_k@mail.ru>
2022-10-03 16:45:06 +0300

    fix checkpoint path

--- core/train_scripts/debertav3_training.py
+++ core/train_scripts/debertav3_training.py
@@ -1,8 +1,9 @@
 from core.experiments.debertav3_experiments import (  # noqa: F401
     experiment_1,
     experiment_2,
+    experiment_3,
 )
 
 if __name__ == "__main__":
     # experiment_1()
-    experiment_2()
+    experiment_3()

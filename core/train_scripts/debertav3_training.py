a3d1cfd58e4cb44bfa7b178af892c014191c6714
dmitrymailk <dmitrymail_k@mail.ru>
2022-10-04 17:37:06 +0300

    add experiment

--- core/train_scripts/debertav3_training.py
+++ core/train_scripts/debertav3_training.py
@@ -6,10 +6,11 @@ from core.experiments.debertav3_experiments import (  # noqa: F401
     experiment_5,
     experiment_6,
     experiment_7,
+    experiment_8,
 )
 
 if __name__ == "__main__":
     # experiment_1()
     # experiment_3()
     # experiment_6()
-    experiment_7()
+    experiment_8()

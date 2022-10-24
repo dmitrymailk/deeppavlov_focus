856a85667d12074f31958e37be3eaaa2fa159b63
dmitrymailk <dmitrymail_k@mail.ru>
2022-10-24 08:55:24 +0300

    add new focal loss

--- core/base_models/mpnet_models.py
+++ core/base_models/mpnet_models.py
@@ -11,7 +11,35 @@ from transformers.models.mpnet.modeling_mpnet import (
 
 import torch
 from torch import nn
-from torchvision.ops import sigmoid_focal_loss
+import torch.nn.functional as F
+
+
+class FocalLossV1(nn.CrossEntropyLoss):
+    """Focal loss for classification tasks on imbalanced datasets"""
+
+    # https://gist.github.com/f1recracker/0f564fd48f15a58f4b92b3eb3879149b
+    def __init__(
+        self,
+        gamma,
+        alpha=None,
+        ignore_index=-100,
+    ):
+        super().__init__(
+            weight=alpha,
+            ignore_index=ignore_index,
+            reduction="mean",
+        )
+        self.gamma = gamma
+
+    def forward(self, input_, target):
+        cross_entropy = super().forward(input_, target)
+        # Temporarily mask out ignore index to '0' for valid gather-indices input.
+        # This won't contribute final loss as the cross_entropy contribution
+        # for these would be zero.
+        target = target * (target != self.ignore_index).long()
+        input_prob = torch.gather(F.softmax(input_, 1), 1, target.unsqueeze(1))
+        loss = torch.pow(1 - input_prob, self.gamma) * cross_entropy
+        return torch.mean(loss)
 
 
 class MPNetForSequenceClassificationV1(MPNetForSequenceClassification):
@@ -87,6 +115,10 @@ class MPNetForSequenceClassificationV2(MPNetForSequenceClassification):
         self.classifier = MPNetClassificationHead(config)
         self.cross_entropy_loss_weights = cross_entropy_loss_weights
 
+        self.loss_fcn = FocalLossV1(
+            gamma=2.0,
+            alpha=cross_entropy_loss_weights,
+        )
         # Initialize weights and apply final processing
         self.post_init()
 
@@ -122,7 +154,10 @@ class MPNetForSequenceClassificationV2(MPNetForSequenceClassification):
 
         loss = None
         if labels is not None:
-            loss = sigmoid_focal_loss(logits.view(-1, self.num_labels), labels.view(-1))
+            loss = self.loss_fcn(
+                logits.view(-1, self.num_labels),
+                labels.view(-1),
+            )
 
         return SequenceClassifierOutput(
             loss=loss,  # type: ignore
